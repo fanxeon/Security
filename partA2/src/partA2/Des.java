294e971749b01a012d4f212b76d8674c307f0cb7
fanxeon <xuanf@student.unimelb.edu.au>
2015-10-04 09:56:01 +1100

    push

--- partA2/src/partA2/Des.java
+++ partA2/src/partA2/Des.java
@@ -101,7 +101,20 @@ public class Des {
 	        return new String(cipher.doFinal(Base64.getDecoder().decode(data.getBytes("UTF-8"))));
 	    }
 
-	    public 
+	    public static void des_encypt (){
+	        final byte[] IP = { //Input Permutation
+	            58, 50, 42, 34, 26, 18, 10, 2,
+	            60, 52, 44, 36, 28, 20, 12, 4,
+	            62, 54, 46, 38, 30, 22, 14, 6,
+	            64, 56, 48, 40, 32, 24, 16, 8,
+	            57, 49, 41, 33, 25, 17, 9,  1,
+	            59, 51, 43, 35, 27, 19, 11, 3,
+	            61, 53, 45, 37, 29, 21, 13, 5,
+	            63, 55, 47, 39, 31, 23, 15, 7
+	        };
+	        
+	    }
+	    
 
 }
 
